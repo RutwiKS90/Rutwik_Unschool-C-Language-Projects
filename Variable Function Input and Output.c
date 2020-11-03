bfdc2eff568a7d5c3c6af0bcd83a5a3a87ccbba3
RutwiKS90 <69425390+RutwiKS90@users.noreply.github.com>
2020-11-03 18:04:09 +0530

    Variable Function
    This is my Unschool C Language Assignment 2

---
+++ Variable Function Input and Output.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int num=0;
+    printf("Enter a number:");
+    scanf("%d",&num);//This function is used to take input from user
+    printf("The number is %d",num);
+    return 0;
+}
